47343cd2965473d6ed458faf7637c824784396e7
Rajaram Joshi <rjoshi14899@gmai.com>
2021-01-20 18:38:12 +0530

    audio

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.less'
 import db from '../firebase';
+import firebase from "firebase";
+import { userStore } from './Store';
+
 
 export default function Player({ id }) {
+    const autUser = userStore.useState((s) => s.user);
+
     const [playing, setplaying] = useState({ title: "loading..." })
     useEffect(() => {
         db.collection("audio")
@@ -12,7 +17,22 @@ export default function Player({ id }) {
                 let data = snap.data();
                 setplaying(data)
             }));
-    })
+        console.log(autUser.uid, autUser.displayName)
+        db.collection("audio")
+            .doc(id)
+            .collection("viewers")
+            .doc(autUser.uid)
+            .get()
+            .then(doc => {
+                console.log(doc.exists, doc.ref)
+                if (!doc.exists) {
+                    doc.ref.set({ id: autUser.uid, name: autUser.displayName, time: firebase.firestore.Timestamp.now() })
+                    db.collection("audio")
+                        .doc(id)
+                        .update({ views: firebase.firestore.FieldValue.increment(1) })
+                }
+            })
+    }, [])
 
     return (
         <div>
@@ -24,11 +44,17 @@ export default function Player({ id }) {
                 src={`https://docs.google.com/uc?export=download&id=${id}`}
                 onPlay={e => console.log("onPlay")}
             />
-            <br/>
+            <br />
             <center>
-            <h2>{playing.title}</h2>
-                </center>
+                <h2>{playing.title}</h2>
+            </center>
             <p>
+                {playing.views && playing.views+10} watched
+                <br />
+                <br />
+                <h4>
+                    Description:-
+                </h4>
                 {
                     playing.description && playing.description.split('I').map(function (item, idx) {
                         return (
