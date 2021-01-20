12927e23049df27b242d0019e4c8799ff6f6d231
Rajaram Joshi <rjoshi14899@gmai.com>
2021-01-20 11:34:39 +0530

    audio

--- src/components/Audios.jsx
+++ src/components/Audios.jsx
@@ -1,20 +1,66 @@
 import { Link } from '@reach/router'
 import { Button, Card } from 'antd'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import db from '../firebase';
 
-export default function Audios() {
+export default function audio() {
+    const [audio, setaudio] = useState([]);
+    const [lastdoc, setlastdoc] = useState({})
+    useEffect(() => {
+        db.collection("audio")
+            .orderBy("date", "desc")
+            .limit(1)
+            .get()
+            .then(async (snap) => {
+                let documents = await snap.docs.map((post) => {
+                    let data = post.data();
+                    console.log(data)
+                    return {
+                        ...data,
+                    };
+                });
+                console.log("documents", documents)
+                setaudio(documents);
+                setlastdoc( snap.docs[snap.docs.length-1])
+            })
+
+    }, []);
+
+    const lookMore = () => {
+        db.collection("audio")
+            .orderBy("date", "desc")
+            .startAt(lastdoc)
+            .limit(1)
+            .get()
+            .then(async (snap) => {
+                let documents = await snap.docs.map((post) => {
+                    let data = post.data();
+                    return {
+                        ...data,
+                    };
+                });
+                console.log("documents", documents)
+                setaudio([...audio, ...documents]);
+                setlastdoc(documents[0])
+                console.log(lastdoc)
+            })
+    }
+
     return (
         <div>
-            <div style={{ marginTop: 20 }}>
-                <Card title={"TITLE"}>
-                    <p>
-                        DESCRIPTION
-        </p>
-                    <Link to={`/audio/1U0WT4ohvFx11RP1nz32MXCE4yKzEJjQE`}>
-                        <Button>PLAY</Button>
-                    </Link>
-                </Card>
-            </div>
+            {
+                audio.map(a => <div style={{ marginTop: 20 }}>
+                    <Card title={a.title}>
+                        <p>
+                            {a.description}
+                        </p>
+                        <Link to={`/audio/${a.id}`}>
+                            <Button>PLAY</Button>
+                        </Link>
+                    </Card>
+                </div>)
+            }
+            <Button onClick={lookMore}>More</Button>
         </div>
     )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.less'
+import db from '../firebase';
 
 export default function Player({ id }) {
+    const [playing, setplaying] = useState({ title: "loading..." })
+    useEffect(() => {
+        db.collection("audio")
+            .doc(id)
+            .onSnapshot((async (snap) => {
+                let data = snap.data();
+                setplaying(data)
+            }));
+    })
+
     return (
         <div>
-            
-                <AudioPlayer
-                    header={<center><h3>Now Playing!!..</h3>   </center>}
-                    customAdditionalControls={[]}
-                    src={`https://docs.google.com/uc?export=download&id=${id}`}
-                    onPlay={e => console.log("onPlay")}
-                />
-                {id}
-         
+
+            <AudioPlayer
+
+                header={<center><h3>Now Playing:- {playing.title}.</h3>   </center>}
+                customAdditionalControls={[]}
+                src={`https://docs.google.com/uc?export=download&id=${id}`}
+                onPlay={e => console.log("onPlay")}
+            />
+            <br/>
+            <h2>{playing.title}</h2>
+            <p>
+                {
+                    playing.description && playing.description.split('I').map(function (item, idx) {
+                        return (
+                            <span key={idx}>
+                                {item}
+                                <br />
+                            </span>
+                        )
+                    })
+                }
+            </p>
+
         </div>
     )
 }
+
+
